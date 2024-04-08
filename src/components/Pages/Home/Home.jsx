357721db24d3636b2a725fe4d8fb51da94e75b22
Artur-Manukyabn <arturmanukyan019@gmail.com>
2024-04-08 15:33:23 +0400

    categories

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Home.scss";
 export default function Home() {
-  return <div>Home</div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios("http://localhost:3000/musical_instruments").then((res) => {
+      console.log(res);
+    });
+  }, []);
+
+  return (
+    <div className="Home">
+      <div className="Home__posters">
+        <div className="Home__poster">
+          <img src="" alt="" />
+        </div>
+        <div className="Home__poster">
+          <img src="" alt="" />
+        </div>
+        <div className="Home__poster">
+          <img src="" alt="" />
+        </div>
+      </div>
+      <h1 className="Home__title">Categires</h1>
+      <div className="Home__cards">
+        <div className="Home__card">
+          <img src="./images/electrick_guitar.jpg" alt="" />
+          <h3>Guitars</h3>
+        </div>
+        <div className="Home__card">
+          <img src="./images/electrick_guitar.jpg" alt="" />
+          <h3>Guitars</h3>
+        </div>
+        <div className="Home__card">
+          <img src="./images/electrick_guitar.jpg" alt="" />
+          <h3>Guitars</h3>
+        </div>
+        <div className="Home__card">
+          <img src="./images/electrick_guitar.jpg" alt="" />
+          <h3>Guitars</h3>
+        </div>
+        <div className="Home__card">
+          <img src="./images/electrick_guitar.jpg" alt="" />
+          <h3>Guitars</h3>
+        </div>
+        <div className="Home__card">
+          <img src="./images/electrick_guitar.jpg" alt="" />
+          <h3>Guitars</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
