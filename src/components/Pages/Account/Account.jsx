bc6dd6863ac206d3cb7f8bf385081ad4fde2f296
Artur-Manukyabn <arturmanukyan019@gmail.com>
2024-04-09 00:32:35 +0400

    account

--- src/components/Pages/Account/Account.jsx
+++ src/components/Pages/Account/Account.jsx
@@ -3,17 +3,37 @@ import React, { useState, useEffect } from "react";
 import "./Account.scss";
 
 export default function Account() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const [history, setHistory] = useState([]);
+  
   useEffect(() => {
     axios("http://localhost:3000/users").then((res) => {
       setUser(res.data[0]);
     });
+    axios("http://localhost:3000/musical_instruments").then((res) => {
+      setProducts(res.data);
+    });
+    setFavorites(
+      products.filter((prod) => {
+        return user.fav.includes(Number(prod.id));
+      })
+    );
+    setHistory(
+      ()=>{
+       products.filter((prod) => {
+          return user.purchases.includes(Number(prod.id));
+        })
+      }
+    );
   }, []);
+
   return (
     <div className="Account">
       <div className="Account__card">
-        <i className="bi bi-pencil-fill"></i>
-        <img src={user.image} alt="" />
+        <i className="bi bi-pencil-fill Account__edit"></i>
+        <img src={user.image} alt="account image" />
         <h2>
           <i className="bi bi-person-fill"></i>
           {user.username}
@@ -26,7 +46,13 @@ export default function Account() {
           Sign Out<i className="bi bi-box-arrow-left"></i>
         </button>
       </div>
-      <div className="Account__purchases"></div>
+      <div className="Account__purchases">
+        <h2>Order history</h2>
+        <div className="Accout__orderHistory">
+        </div>
+        <h2>Favorites</h2>
+        <div className="Account__favorites"></div>
+      </div>
     </div>
   );
 }
