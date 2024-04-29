8bce67af937b4c5e783bb9257a3be6f487903c7d
Artur-Manukyabn <arturmanukyan019@gmail.com>
2024-04-29 22:31:04 +0400

    account

--- src/components/Pages/Account/Account.jsx
+++ src/components/Pages/Account/Account.jsx
@@ -7,7 +7,7 @@ export default function Account() {
   const [products, setProducts] = useState([]);
   const [favorites, setFavorites] = useState([]);
   const [history, setHistory] = useState([]);
-  
+
   useEffect(() => {
     axios("http://localhost:3000/users").then((res) => {
       setUser(res.data[0]);
@@ -15,23 +15,26 @@ export default function Account() {
     axios("http://localhost:3000/musical_instruments").then((res) => {
       setProducts(res.data);
     });
+  }, []);
+
+  useEffect(() => {
     setFavorites(
       products.filter((prod) => {
         return user.fav.includes(Number(prod.id));
       })
     );
+  }, [user, products]);
+  useEffect(() => {
     setHistory(
-      ()=>{
-       products.filter((prod) => {
-          return user.purchases.includes(Number(prod.id));
-        })
-      }
+      products.filter((prod) => {
+        return user.purchases.includes(Number(prod.id));
+      })
     );
-  }, []);
+  }, [user, products]);
 
   return (
     <div className="Account">
-      <div className="Account__card">
+      <div className="Account__personeCard">
         <i className="bi bi-pencil-fill Account__edit"></i>
         <img src={user.image} alt="account image" />
         <h2>
@@ -48,10 +51,16 @@ export default function Account() {
       </div>
       <div className="Account__purchases">
         <h2>Order history</h2>
-        <div className="Accout__orderHistory">
+       
+        <div className="Account__orderHistory">
+        
         </div>
         <h2>Favorites</h2>
-        <div className="Account__favorites"></div>
+        <div className="Account__favorites">
+          {favorites.map((prod) => {
+          return <img className="Account__image" src={prod.image} alt="" />;
+        })}
+        </div>
       </div>
     </div>
   );
